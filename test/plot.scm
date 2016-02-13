801e6bf275db2a20c03d28fb871a8e5a23a208fd
hiroki yagi <hiro.yagi0245@gmail.com>
2016-02-13 16:24:07 +0900

    new test file

--- test/plot.scm
+++ test/plot.scm
@@ -3,4 +3,12 @@
 (define gp (new-gp))
 (define x-lst (iota 100 0 0.1))
 (define y-lst (map sin x-lst))
-(gp-plot-list gp x-lst y-lst)
+(gp-plot-list gp x-lst y-lst
+              #:title "sin"
+              #:with "lines")
+
+(gp-plot-list gp x-lst (reverse y-lst) #:replot #t)
+
+(gp-plot-list gp x-lst (map (lambda (x) (* x x)) y-lst)
+              #:replot #t
+              #:with "lines")
